e838f9b2885bb575dbc1b423cb2a119b5985b466
LihiShalmon <94780547+LihiShalmon@users.noreply.github.com>
2022-04-02 00:59:45 +0300

    Updated ex1 and the first labs

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -21,22 +21,40 @@ def load_data(filename: str) -> pd.DataFrame:
     -------
     Design matrix and response vector (Temp)
     """
-    raise NotImplementedError()
 
+    import sys
+    sys.path.append("../")
+    df = pd.read_csv(filename, parse_dates=['Date'])
+    df['DayOfYear'] = df['Date'].dt.dayofyear
+    return df
 
 if __name__ == '__main__':
     np.random.seed(0)
     # Question 1 - Load and preprocessing of city temperature dataset
-    raise NotImplementedError()
-
+    sys_str = 'C://Users//user//Documents//Uni/year B/IML/IML.HUJI/datasets/City_Temperature.csv'
+    df = load_data(sys_str)
     # Question 2 - Exploring data for specific country
-    raise NotImplementedError()
+    israel_dataset = df[df['Country'] == 'Israel']
+    israel_dataset.sort_values(["DayOfYear"])
+    israel_dataset["Year_Str"] = israel_dataset["Year"].astype(str)
+
+    fig1 = px.scatter(israel_dataset, x='DayOfYear', y='Temp', color='Year_Str')
+    fig1.update_yaxes(range=[0, 30])
+    fig1.show()
+    # what polynomial degree might be suitable for the data
+    for i in range(0, 2):
+        mymodel = np.poly1d(np.polyfit(israel_dataset['DayOfYear'], israel_dataset['Temp'], 3))
+        myline = np.linspace(1, 365, 1)
+        fig1.plot(myline, mymodel(myline))
+        fig1.show()
+
+#    raise NotImplementedError()
 
     # Question 3 - Exploring differences between countries
-    raise NotImplementedError()
+#    raise NotImplementedError()
 
     # Question 4 - Fitting model for different values of `k`
-    raise NotImplementedError()
+ #   raise NotImplementedError()
 
     # Question 5 - Evaluating fitted model on different countries
-    raise NotImplementedError()
+  #  raise NotImplementedError()
